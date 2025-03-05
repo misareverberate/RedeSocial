98762c11b6ac2ae5650f9eef0d1298f33d61381f
Misa Reverberate <misareverberate@gmail.com>
2025-03-04 22:35:34 -0400

    Configurando TailwindCSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
+import { BrowserRouter as Router } from "react-router";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={client }>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
+  </StrictMode>
+);
